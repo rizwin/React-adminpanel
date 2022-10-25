798c6250d31fd9de9253e65fded99d250c65c7bd
rizwin <rizwinazeez@gmail.com>
2022-10-25 13:25:26 +0530

    Add files via upload

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,38 @@
+import "./home.scss"
+
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widgets/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
+const Home = () => {
+    return(
+        <div className="home">
+        <Sidebar/>
+        <div className="homecontainer">
+        <Navbar/>
+        <div className="widgets">
+        <Widget type="user"/>
+        <Widget type="order"/>
+        <Widget type="earning"/>
+        <Widget type="balance"/>
+        </div>
+        <div className="charts">
+        <Featured />
+        <Chart/>
+
+        </div>
+        <div className="listcontainer">
+            <div className="listTitle">
+                Latest Transactions
+                <Table/>
+            </div>
+        </div>
+        </div>
+
+        </div>
+    );
+};
+
+export default Home
